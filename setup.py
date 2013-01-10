3deaddc5f14b71f2772aee70ca684b514a4f8ac3
Robert Niederreiter <office@squarewave.at>
2013-01-10 14:40:55 +0100

    housekeeping

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-from setuptools import setup, find_packages
-import sys, os
+import os
+from setuptools import (
+    setup,
+    find_packages,
+)
+
 
 version = '1.0'
 shortdesc ="AGX XML Input/Output"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 
+
 setup(name='node.ext.xml',
       version=version,
       description=shortdesc,
       long_description=longdesc,
       classifiers=[
-            'Development Status :: 3 - Alpha',
-            'License :: OSI Approved :: GNU General Public License (GPL)',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',     
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',     
       ],
       keywords='AGX, Code Generator, XML IO',
       author='BlueDynamics Alliance',
@@ -22,7 +27,7 @@ setup(name='node.ext.xml',
       url=u'https://svn.plone.org/svn/archetypes/AGX',
       license='GNU General Public Licence',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['node', 'node.ext'],
       include_package_data=True,
       zip_safe=False,
@@ -40,5 +45,4 @@ setup(name='node.ext.xml',
       ),
       entry_points="""
       # -*- Entry points: -*-
-      """,
-      )
+      """)
